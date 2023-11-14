ac85672b80542c2cf3720b7fe818ca2a8fbfdc45
Chandan <cchandel@uwo.ca>
2023-11-14 10:34:12 -0500

    fixed the scaling defect

--- Private Image Annotator/Image Annotator.py
+++ Private Image Annotator/Image Annotator.py
@@ -20,12 +20,15 @@ class ImageAnnotator:
         self.canvas = tk.Canvas(root)
         self.canvas.pack()
 
+
+         # Add a label
+        self.label = tk.Label(root, text="Your Label Text")
+        self.label.pack(side=tk.TOP)
+
         # Load the first image
         self.load_image()
-
+        self.current_index += 1
         # Bind keyboard and mouse events
-        # root.bind("<Left>", self.prev_image)
-        # root.bind("<Right>", self.next_image)
         self.canvas.bind("<ButtonPress-1>", self.start_drawing_rectangle)
         self.canvas.bind("<B1-Motion>", self.drawing_rectangle)
         self.canvas.bind("<ButtonRelease-1>", self.finish_drawing_rectangle)
@@ -35,6 +38,9 @@ class ImageAnnotator:
         image_path = os.path.join(self.image_folder, self.image_list[self.current_index])
         self.image = cv2.imread(image_path)
         self.display_image()
+        self.label.destroy()
+        self.label = tk.Label(root, text= self.image_list[self.current_index])
+        self.label.pack(side=tk.TOP)
 
     def display_image(self):
         image_rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
@@ -55,35 +61,43 @@ class ImageAnnotator:
     def prev_image(self, event):
         self.current_index = max(0, self.current_index - 1)
         self.load_image()
+        self.clear_rectangles()
 
     def next_image(self):
         self.save_labels()
         if  self.current_index < len(self.image_list) - 1:
             self.load_image()
+            self.clear_rectangles()
         else:
             root.destroy()
         self.current_index += 1
 
     def start_drawing_rectangle(self, event):
         self.start_x, self.start_y = event.x, event.y
+        print("starting_x: ", event.x, "starting_y: ", event.y)
 
     def drawing_rectangle(self, event):
         self.canvas.delete(self.current_rectangle)
         self.current_rectangle = self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y)
 
     def finish_drawing_rectangle(self, event):
+        print("ending_x: ", event.x, "ending_y: ", event.y)
         min_x = min(self.start_x, event.x)
         max_x = max(self.start_x, event.x)
         min_y = min(self.start_y, event.y)
         max_y = max(self.start_y, event.y)
         self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y)
-        self.rectangles.append([min_x, min_y, max_x, max_y])
+        self.rectangles.append([min_x, min_y, max_x - min_x, max_y - min_y])
+        print("rectangle_x", min_x, "rectangle_y", min_y, "width", max_x - min_x, "height", max_y - min_y )
+        image_width, image_height = self.image.shape[1] * 0.5, self.image.shape[0] *0.5
+        print("normalized x", min_x/image_width, "normalized y", min_y/image_height, "normalized width", (max_x - min_x)/image_width,"normalized height", (max_y - min_y)/image_height )
 
     def save_labels(self):
         # Save labels in YOLO format
-        image_width, image_height = self.image.shape[1], self.image.shape[0]
-
-        with open(os.path.join(label_folder, self.image_list[self.current_index][:-4]+".txt"), 'w') as file:
+        image_width, image_height = self.image.shape[1] * 0.5, self.image.shape[0] * 0.5
+        
+        print("labels image width: ", image_width, "labels image height: ", image_height)
+        with open(os.path.join(label_folder, self.image_list[self.current_index - 1][:-4]+".txt"), 'w') as file:
             for rect_coords in self.rectangles:
                 x, y, w, h = rect_coords
 
@@ -92,7 +106,7 @@ class ImageAnnotator:
                 y_center = (y + h / 2) / image_height
                 width = w / image_width
                 height = h / image_height
-
+                print("normalized x center", x_center, "normalized y center", y_center, "normalized width", width, "normalized height", height )
                 label = f"0 {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f} \n"
                 file.write(label)
         self.rectangles = []
@@ -113,6 +127,8 @@ if __name__ == "__main__":
     if image_folder:
         annotator = ImageAnnotator(root, image_folder, label_folder)
         
+
+
         # Additional buttons for clearing and saving annotations
         clear_button = tk.Button(root, text="Clear Rectangles", command=annotator.clear_rectangles)
         clear_button.pack(side=tk.TOP)
--- Surgical Tool - Object Detection/YOLOv8 - Detecting Surgical Tools.py
+++ Surgical Tool - Object Detection/YOLOv8 - Detecting Surgical Tools.py
@@ -1,13 +1,14 @@
 from ultralytics import YOLO
 
 # Load a COCO-pretrained YOLOv8n model
-model = YOLO('yolov8n.pt')
+model = YOLO("C:\\Users\\cchan\\computer-vision\\runs\\detect\\train5\\weights\\best.pt") # YOLO('yolov8s.pt')
 
 # Display model information (optional)
 model.info()
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data='data.yaml', epochs=10, imgsz=640)
+# results = model.train(data='data.yaml', epochs=100, imgsz=640, verbose = True, save_dir="C:\\Users\\cchan\\Laproscopic Surgery Work\\Surgical Tool - Object Detection\\runs\\detect", patience = 20)
 
 # Run inference with the YOLOv8n model on the test image
-results = model('C:\\Users\\cchan\\Laproscopic Surgery Work\\Surgical Tool - Object Detection\\train\\images\\002_jpg.rf.8f460f3798190c7107d647ba4eb12ec7.jpg')
+model.predict('C:\\Users\\cchan\\Laproscopic Surgery Work\\Private Image Annotator\\data\\images\\015.jpg', save=True, imgsz=320, conf=0.35)
+model.predict('C:\\Users\\cchan\\Laproscopic Surgery Work\\Private Image Annotator\\data\\images\\023.jpg', save=True, imgsz=320, conf=0.7)
