d433926c56402091ab6a95036dd21bff32e0add6
Chandan <cchandel@uwo.ca>
2023-12-06 21:17:59 -0500

    History and validate works, patience needs a little work, but pretty close.

--- Surgery Images - Temporal Ordering/Siamese Networks - Image Time Comparison.py
+++ Surgery Images - Temporal Ordering/Siamese Networks - Image Time Comparison.py
@@ -5,8 +5,20 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras import layers, models, applications
+import matplotlib.pyplot as plt
+import sys, time
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+def loading_bar(total, completed):
+    progress = int((completed / total) * 100)
+    toolbar_width = 100
+    progress_length = int(toolbar_width * (completed / total))
+    sys.stdout.write("[%s%s] %d%%" % ("=" * progress_length, " " * (toolbar_width - progress_length), progress))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width + 6))  # return to start of line, after ']'
+
 
 # Load and preprocess images
 def load_and_preprocess_image(image_path):
@@ -31,15 +43,23 @@ def data_generator(image_paths_1, image_paths_2, labels, batch_size=32):
                         'input_2': [np.array(batch_images_2), np.array(batch_paths_2)]
                     }, np.array(batch_labels)
 
-def custom_fit(siamese_model, yolo_model, num_epochs=10, steps_per_epoch=100, batch_size = 32):
+def custom_fit(siamese_model, yolo_model, num_epochs=10, steps_per_epoch=100, batch_size = 64, patience = 3):
     optimizer = tf.keras.optimizers.Adam()
     loss_fn = tf.keras.losses.BinaryCrossentropy()
-
+    # Early Stopping Callback
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=patience, restore_best_weights=True
+    )
+    history = {'loss': [], 'accuracy': [], 'val_loss': [], 'val_accuracy': []}  # Create an empty history dictionary
     for epoch in range(num_epochs):
-        print(f"Epoch {epoch + 1}/{num_epochs}")
-        data_gen = data_generator(train_image_paths_1, train_image_paths_2, train_labels, batch_size)
+        print(f"Training Epoch {epoch + 1}/{num_epochs}")
+        per_epoch_history = {'loss': [], 'accuracy': [], 'val_loss': [], 'val_accuracy': []}  # Create an empty history dictionary
+        train_data_gen = data_generator(train_image_paths_1, train_image_paths_2, train_labels, batch_size)
+        test_data_gen = data_generator(test_image_paths_1, test_image_paths_2, test_labels, batch_size)
+        #Training loop
         for step in range(steps_per_epoch):
-            inputs, labels = next(data_gen)
+            loading_bar(steps_per_epoch, step)
+            inputs, labels = next(test_data_gen)
             
             input_1_images, input_1_paths = inputs['input_1']
             input_2_images, input_2_paths = inputs['input_2']
@@ -74,9 +94,59 @@ def custom_fit(siamese_model, yolo_model, num_epochs=10, steps_per_epoch=100, ba
                 # Compute gradients and update weights
                 gradients = tape.gradient(loss, siamese_model.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, siamese_model.trainable_variables))
+                per_epoch_history['loss'].append(loss)
+                per_epoch_history['accuracy'].append(accuracy)
+ 
+        # Validation loop, I want to see each step, so to make the graph smooth I will recaulte batch size and hold the number of val_steps+per_epoch constant
+        val_steps_per_epoch = len(test_image_paths_1) //  batch_size # Adjust for your validation data length
+        print(f"Validation Epoch {epoch + 1}/{num_epochs}")
+        for val_step in range(val_steps_per_epoch):
+            loading_bar(val_steps_per_epoch, val_step)
+            val_inputs, val_labels = next(test_data_gen)
+            input_1_images, input_1_paths = val_inputs['input_1']
+            input_2_images, input_2_paths = val_inputs['input_2']
 
-            # Display loss and accuracy
-            print(f"Step {step}/{steps_per_epoch}, Loss: {loss:.4f}, Accuracy: {accuracy:.4f}")
+            # Load and preprocess images for YOLO input
+            yolo_input_1 = [load_img(image_path) for image_path in input_1_paths]
+            yolo_input_2 = [load_img(image_path) for image_path in input_2_paths]
+            
+            # Make YOLO predictions
+            yolo_output_1 = yolo_model.predict(yolo_input_1, save=False, imgsz=640, conf=0.35, verbose = False)
+            yolo_output_2 = yolo_model.predict(yolo_input_2, save=False, imgsz=640, conf=0.35, verbose = False)
+
+            num1 = []
+            num2 = []
+
+            # convert the yolo output to a length tensor
+            for idx in range(batch_size):
+                num1.append([len(yolo_output_1[idx].boxes.xywhn)])
+                num2.append([len(yolo_output_2[idx].boxes.xywhn)])
+            
+            # We are not training the model here, just validating it
+            num1 = np.array(num1)
+            num2 = np.array(num2)
+            val_labels = [[i] for i in val_labels]
+            outputs = siamese_model([input_1_images, input_2_images, num1, num2])
+            val_loss = loss_fn(val_labels, outputs)
+            # Calculate accuracy
+            predicted_labels = [1 if output >= 0.5 else 0 for output in outputs]
+            true_labels = [label[0] for label in val_labels]
+            val_accuracy = np.mean(np.array(predicted_labels) == np.array(true_labels))
+            per_epoch_history['val_loss'].append(val_loss)
+            per_epoch_history['val_accuracy'].append(val_accuracy)
+        # Display loss and accuracy
+        print(f"Step {step}/{steps_per_epoch}, Loss: {loss:.4f}, Validation Loss: {val_loss:.4f}, Accuracy: {accuracy:.4f}, Validation Accuracy: {val_accuracy:.4f}")
+        history['loss'].append(sum(per_epoch_history['loss']) / len(per_epoch_history['loss']))
+        history['val_loss'].append(sum(per_epoch_history['val_loss']) / len(per_epoch_history['val_loss']))
+        history['accuracy'].append(sum(per_epoch_history['accuracy']) / len(per_epoch_history['accuracy']))
+        history['val_accuracy'].append(sum(per_epoch_history['val_accuracy']) / len(per_epoch_history['val_accuracy']))
+        # Check for early stopping
+        if epoch >= patience:  # Check if current epoch is greater than or equal to patience
+            recent_val_losses = history['val_loss'][-(patience + 1):]
+            if all(recent_val_losses[i] > recent_val_losses[i + 1] for i in range(patience)):
+                print(f"Stopping early as validation loss didn't improve for {patience} epochs.")
+                break
+    return history
 
 def create_siamese_model(input_shape):
     input_1 = tf.keras.Input(shape=input_shape)
@@ -121,7 +191,7 @@ test_image_paths_1 = test['img_path_1']
 test_image_paths_2 = test['img_path_2']
 test_labels = test['labels']
 
-training_length = len(train_image_paths_1)
+training_length = len(test_image_paths_1)
 batch_size = 32
 print("training length: ", len(train_image_paths_1), "testing length: ", len(test_image_paths_2))
 
@@ -131,4 +201,26 @@ input_shape = (299, 299, 3)  # Adjust the input shape based on your images
 yolo_model = YOLO("best.pt")
 siamese_model = create_siamese_model(input_shape)
 # custom_fit(siamese_model, yolo_model, data_gen)
-custom_fit(siamese_model, yolo_model, num_epochs=10, steps_per_epoch=training_length//batch_size, batch_size = batch_size)
+history = custom_fit(siamese_model, yolo_model, num_epochs = 30, steps_per_epoch=training_length//batch_size, batch_size = batch_size)
+
+# Plotting the training and validation loss
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(history['loss'], label='Training Loss')
+plt.plot(history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+# Plotting the training and validation accuracy
+plt.subplot(1, 2, 2)
+plt.plot(history['accuracy'], label='Training Accuracy')
+plt.plot(history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
