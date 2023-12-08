6be50f1c28fc46dae0c4dbe09abbb6ee25f1bc61
Chandan Swaraj Chandel <cchandel@uwo.ca>
2023-12-08 12:11:25 -0500

    minor modifications

--- Surgical Tool - Object Detection/YOLOv8 - Detecting Surgical Tools.py
+++ Surgical Tool - Object Detection/YOLOv8 - Detecting Surgical Tools.py
@@ -1,4 +1,4 @@
-from ultralytics import YOLO, settings
+from ultralytics import YOLO
 
 # Load a COCO-pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
@@ -6,15 +6,6 @@ model = YOLO('yolov8n.pt')
 # Display model information (optional)
 model.info()
 
-# Update a setting
-settings.update(
-        {
-            'runs_dir': 'C:\\Users\\cchan\\Laproscopic Surgery Work\\runs\\detect'      
-        }
-    )
-
-# Reset settings to default values
-settings.reset()
 
 # Train the model on the COCO8 example dataset for 100 epochs
 results = model.train(data='data.yaml', epochs=100, imgsz=640, verbose = True, patience = 20)
